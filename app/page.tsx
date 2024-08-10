7f5519927432e27a13f48f3478033853c133bf6e
Sujikathait1 <Sujikathait1@gmail.com>
2024-08-09 22:29:41 -0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,44 @@
+import dynamic from 'next/dynamic';
+import { Button } from "@/components/ui/button";
+import { Section } from "lucide-react";
+import Image from "next/image";
+import Link from 'next/link';
+import e from 'express';
+import PatientForm from '@/components/Forms/PatientForm';
+import PassKeyModal from '@/components/PassKeyModal';
+
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
+
+  return (
+    <div className="flex h-screen max-h-screen">
+      {isAdmin && <PassKeyModal />}
+
+      <Image
+        src="/assets/images/onboarding-img.jpg"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%] float-left"
+      />
+
+      <section className="remove-scroll container my-auto flex-grow">
+        <div className="sub-container max-w-[496px]">
+          <Image 
+            src="/assets/icons/logo-full.svg"
+            height={900}
+            width={900}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+          <div className='text-14-regular mt-20 flex justify-between'>
+            <p className='justify-items-end text-dark-600 xl:text-left'>© 2024 Carepulse</p>
+            <Link href="/?admin=true" className="text-green-500">admin</Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
---
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -0,0 +1,45 @@
+import dynamic from 'next/dynamic';
+import { Button } from "@/components/ui/button";
+import { Section } from "lucide-react";
+import Image from "next/image";
+import Link from 'next/link';
+import e from 'express';
+import PatientForm from '@/components/Forms/PatientForm';
+import AppointmentForm from '@/components/Forms/AppointmentForm';
+import { getPatient } from '@/lib/actions/patient.actions';
+import * as Sentry from '@sentry/nextjs'
+ export default async function NewAppointment({params:{userId}}:SearchParamProps) {
+  const patient = await getPatient(userId);
+  Sentry.metrics.set("user_view_new-appointment", patient.name);
+  return (
+    <div className="flex h-screen max-h-screen">
+ <section className="remove-scroll container my-auto">
+<div className="sub-container max-w-[860px] flex-1 justify-between">
+  <Image src="/assets/icons/logo-full.svg"
+height={1000}
+width={1000}
+alt="patient"
+className="mb-12 h-10 w-fit"/>
+       <AppointmentForm
+            patientId={patient?.$id}
+            userId={userId}
+            type="create" setOpen={function (open: boolean): void {
+              throw new Error('Function not implemented.');
+            } }          />
+
+<div className='text-14-regular mt-20 flex justify-between'>
+<p className='justify-items-end text-dark-600 xl:text-left'> © 2024 Carepulse  </p>
+<Link href= "/?admin=true" className="text-green-500">admin</Link>
+</div>
+</div>
+</section>
+<Image
+src="/assets/images/appointment-img.png"
+height={1000}
+width={1000}
+alt = "patient"
+className="side-img max-w-[50%]"/>
+</div>)
+}
+
+
